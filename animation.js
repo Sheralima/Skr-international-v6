cb9e80e5a760ef6764a50626c19b45c953925b4d
Sheralima <sherofali@gmail.com>
2026-07-22 12:58:17 +0500

    Add scroll animation for elements with 'animate' class
    Implement scroll animation using Intersection Observer.

--- animation.js
+++ animation.js
@@ -1,1 +1,21 @@
+// ===========================
+// Scroll Animation
+// ===========================
 
+const elements = document.querySelectorAll(".animate");
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+
+        if (entry.isIntersecting) {
+            entry.target.classList.add("show");
+        }
+
+    });
+}, {
+    threshold: 0.2
+});
+
+elements.forEach(element => {
+    observer.observe(element);
+});
