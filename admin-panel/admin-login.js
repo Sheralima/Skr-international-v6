c09d7b882213c2feebfefd2fffa4d7f1685498ca
Sheralima <sherofali@gmail.com>
2026-07-22 15:33:39 +0500

    Implement admin login functionality with validation

--- admin-panel/admin-login.js
+++ admin-panel/admin-login.js
@@ -1,1 +1,58 @@
+// ==========================================
+// SKR INTERNATIONAL
+// Admin Login JS
+// ==========================================
 
+console.log("Admin Login Loaded");
+
+// Form
+const loginForm = document.getElementById("adminLoginForm");
+
+// Email
+const email = document.getElementById("adminEmail");
+
+// Password
+const password = document.getElementById("adminPassword");
+
+// Show Password
+const showPassword = document.getElementById("showPassword");
+
+if (showPassword) {
+
+    showPassword.addEventListener("change", function () {
+
+        if (this.checked) {
+            password.type = "text";
+        } else {
+            password.type = "password";
+        }
+
+    });
+
+}
+
+// Login
+if (loginForm) {
+
+    loginForm.addEventListener("submit", function (e) {
+
+        e.preventDefault();
+
+        if (
+            email.value === "admin@skr.com" &&
+            password.value === "123456"
+        ) {
+
+            alert("Login Successful");
+
+            window.location.href = "admin-dashboard.html";
+
+        } else {
+
+            alert("Invalid Email or Password");
+
+        }
+
+    });
+
+}
