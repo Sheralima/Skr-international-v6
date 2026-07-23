b4ecfff54f8859f677a7e640312aabed79b8723d
Sheralima <sherofali@gmail.com>
2026-07-23 14:16:47 +0500

    Implement auto image slider functionality

--- J's/slider.js
+++ J's/slider.js
@@ -1,1 +1,31 @@
+// ===========================
+// Auto Image Slider
+// ===========================
 
+const slides = documenconsole.log("Slider Loaded");t.querySelectorAll(".slide");
+let currentSlide = 0;
+
+function showSlide(index) {
+    slides.forEach(slide => {
+        slide.classList.remove("active");
+    });
+
+    slides[index].classList.add("active");
+}
+
+if (slides.length > 0) {
+
+    showSlide(currentSlide);
+
+    setInterval(() => {
+        currentSlide++;
+
+        if (currentSlide >= slides.length) {
+            currentSlide = 0;
+        }
+
+        showSlide(currentSlide);
+
+    }, 3000); // 3 seconds
+
+}
